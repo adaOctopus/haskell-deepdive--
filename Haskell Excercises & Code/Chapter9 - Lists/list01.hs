136fca687fe0a4bda47d991a81aa36f01405d68d
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-08-12 15:04:22 +0300

    Adding more excercises on list for ranging

--- Haskell Excercises & Code/Chapter9 - Lists/list01.hs
+++ Haskell Excercises & Code/Chapter9 - Lists/list01.hs
@@ -3,4 +3,24 @@ module ListOne where
 -- Data type declaration of lists
 -- data [] a = [] | a : [a]
 
-myHead :: [a] -> a
+myHead :: [a] -> Maybe a
+myhead []     = Nothing
+myHead [x]    = Just x
+myHead (x:xs) = Just x
+
+-- 9.5 excercises
+eftBool :: Bool -> Bool -> [Bool]
+eftBool True True   = True : [] ++ True : []
+eftBool True False  = True : [] ++ False : []
+eftBool False True  = False : [] ++ True : []
+eftBool False False = False : [] ++ False : []
+eftBool _ _         = error "Please pass Booleans"
+
+-- Maybe here it means to do ranging as in, if we pass 1 10, to not do just [1,10] concatenation
+    -- But instead do [1,2,3,4,5,6,7,8,9,10]
+    -- I did not understand from the context
+eftInt :: Int -> Int -> [Int]
+eftInt x y = x : [] ++ y : []
+
+
+
