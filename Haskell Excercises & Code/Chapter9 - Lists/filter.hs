bbc1ed5dc08428fc832f547fbe97b6f268706040
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-08-13 17:49:18 +0300

    Completed exer in 9.10

---
+++ Haskell Excercises & Code/Chapter9 - Lists/filter.hs
@@ -0,0 +1,9 @@
+module Filtering where
+
+
+-- 9.11 Write a function that returns all 3 multiples of 1..30
+
+myFunc1 = length . filter (\x -> x `mod` 3 == 0) $ [1..60]
+
+-- Using `words` split a string into list of words and remove from a sentence the -> "the" "a" "an"
+myFunc2 = filter (\x -> x `notElem` ["the", "a","an"]) . words $ "the brown dog a was not bad an good"
