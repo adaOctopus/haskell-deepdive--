f7dd1a696ef694bca473b632e145a958c79d2ef5
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-08-14 16:41:34 +0300

    Making all upper with Recursion style

--- Haskell Excercises & Code/Chapter9 - Lists/chapterEx.hs
+++ Haskell Excercises & Code/Chapter9 - Lists/chapterEx.hs
@@ -12,3 +12,18 @@ takeUpper word = filter (\x -> isUpper x) word
 -- Write a function that will capitalize the first letter of a string and return the entire string.
 makeItUpper :: String -> String
 makeItUpper word = (toUpper . head $ word) : (tail word)
+
+
+-- Write a function that will capitalize alll letters of a word
+-- allUpper :: String -> String
+allUpper ""   = ""
+allUpper (x:xs) = (toUpper x) : allUpper xs
+
+-- nikos
+-- n   ikos :: get the head
+-- N   ikos :: capitalize head
+-- N   I   kos :: head + head of tail + new_tail
+-- N   I   K   os :: head + head of tail + head of new tail + new new tail
+-- N   I   K   O   s
+-- N   I   K   O   S
+-- NIKOS
