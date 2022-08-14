982ad9dcfe3ca6ea83b6dcf3a59560385bd143da
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-08-14 17:42:06 +0300

    myElem, recursive version of elem function

--- Haskell Excercises & Code/Chapter9 - Lists/rewrite.hs
+++ Haskell Excercises & Code/Chapter9 - Lists/rewrite.hs
@@ -17,3 +17,11 @@ myAny f [x]    = if f x == True then True else False
 myAny f (x:xs) 
   | f x == True = True 
   | f x /= True = myAny f xs
+
+-- 3. recursive version of elem function
+myElem :: Eq a => a -> [a] -> Bool
+myElem x []      = False
+myElem x [n]     = if x == n then True else False 
+myElem k (x:xs) 
+  | k == x = True 
+  | k /= x = myElem k xs
