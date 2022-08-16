1649acbbec6d0030893d96d64188502a559d32d4
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-08-16 13:49:20 +0300

    Extracting utctime data type from list

--- Haskell Excercises & Code/Chapter10 - Folding Lists/database.hs
+++ Haskell Excercises & Code/Chapter10 - Folding Lists/database.hs
@@ -3,3 +3,19 @@ module Database where
 import Data.Time
 
 data DatabaseItem = DbString String | DbNumber Integer | DbDate UTCTime deriving (Eq, Ord, Show)
+
+theDatabase :: [DatabaseItem]
+theDatabase = [ DbDate (UTCTime 
+                          (fromGregorian 1911 5 1)
+                (secondsToDiffTime 34123))
+              , DbNumber 9001
+              , DbString "Hello, world!"
+              , DbDate (UTCTime
+                        (fromGregorian 1921 5 1)
+                        (secondsToDiffTime 34123))
+              ]
+
+-- 1. Write a function that filters for DbDate values and returns
+-- a list of the UTCTime values inside them.
+filterDbDate :: [DatabaseItem] -> [UTCTime]
+filterDbDate (DbDate (UTCTime d dif):_) = [UTCTime d dif]
