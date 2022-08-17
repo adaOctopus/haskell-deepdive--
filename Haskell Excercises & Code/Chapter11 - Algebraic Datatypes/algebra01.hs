33221fa44867488fa11a10bea491f97d525fb457
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-08-17 19:19:31 +0300

    Declared data types, sum & product

--- Haskell Excercises & Code/Chapter11 - Algebraic Datatypes/algebra01.hs
+++ Haskell Excercises & Code/Chapter11 - Algebraic Datatypes/algebra01.hs
@@ -1,3 +1,11 @@
 module Algbraic where 
 
 data Vehicle = Car Manufacturer Price | Plane Airline deriving  (Eq, Show)
+data Manufacturer = Mini | Mazda | Tata deriving (Eq, Show)
+
+data Airline = PapuAir | CatapultsR'Us | TakeYourChancesUnited deriving (Eq, Show)
+
+myCar = Car Mini (Price 14000)
+urCar = Car Mazda (Price 20000)
+clownCar = Car Tata (Price 7000)
+doge = Plane PapuAir
