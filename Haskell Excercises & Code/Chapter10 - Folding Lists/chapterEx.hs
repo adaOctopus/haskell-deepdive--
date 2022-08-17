5977a2cf72f879e0101ea47b7635a17413200db6
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-08-17 12:46:02 +0300

    Reasoning of the seekritFunc explained in steps and example

--- Haskell Excercises & Code/Chapter10 - Folding Lists/chapterEx.hs
+++ Haskell Excercises & Code/Chapter10 - Folding Lists/chapterEx.hs
@@ -24,3 +24,20 @@ tripleTuple = [ (cn, vw, gn) | cn <- stops, vw <- vowels, gn <- reverse stops]
 -- Part B. Only return those starting with `p` 
 -- Basically we only added a guard 
 tripleTupleP = [ (cn, vw, gn) | cn <- stops, vw <- vowels, gn <- reverse stops, cn == 'p']
+
+-- Part C was redundant, and decided to not code it. Code long and prosper.
+
+-- Ex.2 What does the secret function do? What is its type?
+seekritFunc x = div (sum (map length (words x))) (length (words x))
+----------------(A)  (B)  (C)  (D)      (E)(F)    (D)      (E)(F)
+-- Explain components
+-- E+F :: words splits a sentence into a list of words (splits by space)
+-- D :: calculates the length of the list of words , basically how many words
+-- C :: maps length over each word to see how many chars are in the word
+-- A divides the two.
+-- i.e seekritFunc "Niko came back"
+ -- D E F returns number 3 , as 3 words exist in the sentence
+ -- C D E F returns a list of the length of each word :: [4,4,4]
+ -- B (C D E F)::  sums the above so, 4+4+4 = 12
+ -- A :: div 12 3 = 4 run it in your repl and see
+ -- seekritFunc "Niko came back"
