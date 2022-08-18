b21e6b38a64134f52ab2a8f4a2674a00d47a00df
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-08-18 17:17:01 +0300

    Adding Size parameter to the Plane data type

--- Haskell Excercises & Code/Chapter11 - Algebraic Datatypes/algebra01.hs
+++ Haskell Excercises & Code/Chapter11 - Algebraic Datatypes/algebra01.hs
@@ -2,9 +2,10 @@ module Algbraic where
 
 
 data Price = Price Integer deriving (Eq, Show)
+data Size = Size Integer deriving (Eq, Show)
 data Manufacturer = Mini | Mazda | Tata deriving (Eq, Show) 
 data Airline = PapuAir | CatapultsR'Us | TakeYourChancesUnited deriving (Eq, Show)
-data Vehicle = Car Manufacturer Price | Plane Airline deriving  (Eq, Show)
+data Vehicle = Car Manufacturer Price | Plane Airline Size deriving  (Eq, Show)
 
 myCar = Car Mini (Price 14000)
 urCar = Car Mazda (Price 20000)
@@ -22,6 +23,12 @@ isCar (Car Tata (Price n))  = True
 isCar _ = False
 
 isPlane :: Vehicle -> Bool
-isPlane = undefined
+isPlane  (Plane PapuAir (Size n))= True
+isPlane  (Plane CatapultsR'Us  (Size n))= True
+isPlane (Plane TakeYourChancesUnited  (Size n)) = True
+isPlane _ = False
+
 areCars :: [Vehicle] -> [Bool]
 areCars = undefined
+
+-- Reconstructed Plane with a Size parameters.
