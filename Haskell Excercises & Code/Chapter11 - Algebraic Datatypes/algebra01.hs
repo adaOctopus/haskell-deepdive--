23fc21a493c00e03c9b8329de75b2e4df86d27eb
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-08-18 17:11:23 +0300

    Creating algebraric datatypes

--- Haskell Excercises & Code/Chapter11 - Algebraic Datatypes/algebra01.hs
+++ Haskell Excercises & Code/Chapter11 - Algebraic Datatypes/algebra01.hs
@@ -1,11 +1,27 @@
 module Algbraic where 
 
-data Vehicle = Car Manufacturer Price | Plane Airline deriving  (Eq, Show)
-data Manufacturer = Mini | Mazda | Tata deriving (Eq, Show)
 
+data Price = Price Integer deriving (Eq, Show)
+data Manufacturer = Mini | Mazda | Tata deriving (Eq, Show) 
 data Airline = PapuAir | CatapultsR'Us | TakeYourChancesUnited deriving (Eq, Show)
+data Vehicle = Car Manufacturer Price | Plane Airline deriving  (Eq, Show)
 
 myCar = Car Mini (Price 14000)
 urCar = Car Mazda (Price 20000)
 clownCar = Car Tata (Price 7000)
 doge = Plane PapuAir
+
+-- 1. myCar type :: Vehicle
+-- 2. urCar  type :: Vehicle
+
+-- 2. Given the following, define the functions:
+isCar :: Vehicle -> Bool
+isCar (Car Mini (Price n))  = True
+isCar (Car Mazda (Price n))  = True
+isCar (Car Tata (Price n))  = True
+isCar _ = False
+
+isPlane :: Vehicle -> Bool
+isPlane = undefined
+areCars :: [Vehicle] -> [Bool]
+areCars = undefined
