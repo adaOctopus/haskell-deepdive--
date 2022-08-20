6150d6a701a701b62e9a3bb75a13906734f6bea0
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-08-20 15:49:39 +0300

    Advanced list comprehension reasoning understood.

--- Haskell Excercises & Code/Chapter11 - Algebraic Datatypes/programmer.hs
+++ Haskell Excercises & Code/Chapter11 - Algebraic Datatypes/programmer.hs
@@ -27,6 +27,9 @@ myFunca = length allLanguages * length allOperatingSystems
 
 -- try it out
 -- iterate
+-- Practicing to understand how list comprehension works, to generate the product of Programmers, without writting the type
+-- I guess we are clever... ;)
 listOne = [1,2,3,4]
 listTwo = ['a','b','c','d']
-funca   = [(x1,x2) | x1 <- listOne, x2 <- listTwo]
+listThree = ['k','y']
+funca   = [(x1,x2,x3) | x1 <- listOne, x2 <- listTwo, x3 <- listThree]
