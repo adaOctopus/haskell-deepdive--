bdfa3422d8863ef69fa7192e5cf8d8338cd93ff7
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-08-22 16:28:56 +0300

    Filter, unfilter & list comprehension

--- Haskell Excercises & Code/Chapter12 - Signalign Adversity/chapterEx.hs
+++ Haskell Excercises & Code/Chapter12 - Signalign Adversity/chapterEx.hs
@@ -20,4 +20,27 @@ replaceThe senten = unwords checkForThe
     checkForThe = [if i == "the" then "a" else i | i <- listOfWords ]
 
 
+-- 2. Write a recursive function that takes a text/string, breaks
+-- it into words, and counts the number of instances of ”the”
+-- followed by a vowel-initial word.
+
+countTheBeforeVowel :: String -> Int
+countTheBeforeVowel sentence = findThe
+  where
+    listOfWords = words sentence
+    findThe     = length $ filter (=="the") listOfWords
+
+
+-- 3. Count vowels in a word
+-- Completed
+
+vowels = "aeiou"
+
+countVowels :: String -> Int
+countVowels wordi = numbrOfVowels
+  where
+    checkVowels = [ x | x <- wordi, x `elem` vowels]
+    numbrOfVowels = length checkVowels
+
+
 
