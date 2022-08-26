5c40e3f4d17a54cf113df26d8588fc21013d4728
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-08-26 13:25:04 +0300

    Hangman game complete code

--- Haskell Excercises & Code/Chapter13 - Building Projects/Hangman.hs
+++ Haskell Excercises & Code/Chapter13 - Building Projects/Hangman.hs
@@ -94,3 +94,66 @@ renderPuzzleChar lt =
   case lt of
     Nothing -> '_'
     Just c  -> c
+
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle word  filledInSoFar s) c = Puzzle word newFilledInSoFar (c : s)
+  where 
+    zipper guessed wordChar guessChar = 
+      if wordChar == guessed then Just wordChar else guessChar 
+    newFilledInSoFar = zipWith (zipper c) word filledInSoFar
+
+
+handleGuess :: Puzzle -> Char -> IO Puzzle
+handleGuess puzzle guess = do 
+  putStrLn $ "Your guess was: " ++ [guess]
+  case (charInWord puzzle guess, alreadyGuessed puzzle guess) of
+    (_, True) -> do
+              putStrLn "You already guessed that\
+                        \ character, pick \
+                        \ something else!"
+              return puzzle
+    (True, _) -> do
+              putStrLn "This character was in the\
+                      \ word, filling in the word\
+                      \ accordingly"
+              return (fillInCharacter puzzle guess)
+    (False, _) -> do
+              putStrLn "This character wasn't in\
+                      \ the word, try again."
+              return (fillInCharacter puzzle guess)
+
+
+-- Signaling game over after a set number of guesses
+
+gameOver :: Puzzle -> IO ()
+gameOver (Puzzle wordToGuess _ guessed) = if (length guessed) > 7 then do 
+                                                                       putStrLn "You lose!"
+                                                                       putStrLn $ "The word was: " ++ wordToGuess
+                                                                       exitSuccess
+                                          else return ()
+
+-- Signaling Game Win
+
+gameWin :: Puzzle -> IO ()
+gameWin (Puzzle _ filledInSoFar _) = if all isJust filledInSoFar then do 
+                                                                       putStrLn "You win!"
+                                                                       exitSuccess
+                                     else return ()
+
+runGame :: Puzzle -> IO ()
+runGame puzzle = forever $ do
+                            gameOver puzzle
+                            gameWin puzzle
+                            putStrLn $ "Current puzzle is: " ++ show puzzle
+                            putStr "Guess a letter: "
+                            guess <- getLine 
+                            case guess of
+                              [c] -> handleGuess puzzle c >>= runGame
+                              _ -> putStrLn "Your guess must\
+                                             \ be a single character"
+
+main :: IO ()
+main = do
+  word <- randomWord'
+  let puzzle = freshPuzzle (fmap toLower word)
+  runGame puzzle
