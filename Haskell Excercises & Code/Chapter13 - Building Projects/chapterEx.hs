ce52fc4db4b1bb06b2626eaf2a51b80b59dcc3a7
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-08-26 14:11:45 +0300

    Completed exer 3, with advanced palindrome fix

--- Haskell Excercises & Code/Chapter13 - Building Projects/chapterEx.hs
+++ Haskell Excercises & Code/Chapter13 - Building Projects/chapterEx.hs
@@ -4,15 +4,30 @@ module ChapterEx where
 import Control.Monad
 import Data.Char
 
+
+
+-- Modifying Code Ex 3 mysSolution
+-----------------------------------
 capitals = ['A'..'Z']
 lows     = ['a'..'z']
 vowels   = "aeiou"
 
+badChar :: String -> String
+badChar wordi   = [x | x <- wordi, x `notElem` vowels, x `notElem` capitals, x `notElem` lows]
+
+cleanChar :: String -> String
+cleanChar wordi = filter (\x -> x `notElem` badList) wordi
+  where
+    badList = badChar wordi
+
+-----------------------------------
+-----------------------------------
 
 palindrome :: IO ()
 palindrome = do
   line1 <- getLine
-  case (line1 == reverse line1) of
+  let cleanOne = cleanChar line1
+  case (cleanOne == reverse cleanOne) of
    True -> do 
             putStrLn "It's a palindrome!"
             palindrome      
@@ -20,5 +35,3 @@ palindrome = do
              putStrLn "Nope!"
              return ()
 
-returnChar :: String -> [Char]
-returnChar wordi = [x | x <- wordi, x `notElem` vowels, x `notElem` capitals, x `notElem` lows]
