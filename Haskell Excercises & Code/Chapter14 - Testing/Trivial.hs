f88099cf28531b2f0135f22416378e64f47ee64a
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-08-30 14:46:25 +0300

    Trivial with arbitrary Gen a types

---
+++ Haskell Excercises & Code/Chapter14 - Testing/Trivial.hs
@@ -0,0 +1,34 @@
+module Trivial where
+
+import Test.QuickCheck
+import Test.QuickCheck.Gen (oneof)
+
+data Trivial = Trivial deriving (Eq, Show)
+
+
+trivialGen :: Gen Trivial
+trivialGen = return Trivial
+
+instance Arbitrary Trivial where 
+    arbitrary = trivialGen
+
+data Identity a = Identity a deriving (Eq, Show)
+
+identityGen :: Arbitrary a => Gen (Identity a)
+identityGen = do 
+    a <- arbitrary
+    return (Identity a)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+    arbitrary = identityGen
+
+identityGenInt :: Gen (Identity Int)
+identityGenInt = identityGen
+
+data Pair a b = Pair a b deriving (Eq, Show)
+
+pairGen :: (Arbitrary a, Arbitrary b) => Gen (Pair a b)
+pairGen = do 
+    a <- arbitrary
+    b <- arbitrary
+    return (Pair a b)
