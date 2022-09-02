81e16278cd02234a0b9a7f59d68ba67ce0212cd4
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-09-02 14:23:15 +0300

    Testing associativity with quickCheck

--- Haskell Excercises & Code/Chapter15 - Monoid Semigroup/Madness.hs
+++ Haskell Excercises & Code/Chapter15 - Monoid Semigroup/Madness.hs
@@ -1,5 +1,6 @@
 module Madness where
 import Data.Monoid
+import Test.QuickCheck
 
 type Verb = String
 type Adjective = String
@@ -24,3 +25,12 @@ madlibbinBetter' e adv noun adj =
     mconcat [e, "! he said ", adv, " as he jumped into his car ", noun,
              " and drove off with his ", adj, " wife."]
 
+
+-- Using QuickCheck to test associativity
+monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
+monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+-- We have to declare the types for the function in order to
+-- run the tests, so that QuickCheck knows what types of data to
+-- generate.
+
+-- Using quickcheck to test identity
