52c132b9b865953198fe37aad74401c238d76199
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-09-03 17:22:12 +0300

    Quickcheck, semigroup Instances

--- Haskell Excercises & Code/Chapter15 - Monoid Semigroup/chapterEx.hs
+++ Haskell Excercises & Code/Chapter15 - Monoid Semigroup/chapterEx.hs
@@ -10,3 +10,45 @@ import Test.QuickCheck
 --     (<>) Nothing _             = Nothing
 --     (<>) _ Nothing             = Nothing
 --     (<>) (Just one) (Just two) = Just (one <> two) 
+
+-- == Exerc 1.
+data Trivial = Trivial deriving (Eq, Show)
+
+instance Semigroup Trivial where 
+    (<>) Trivial Trivial = Trivial
+
+instance Arbitrary Trivial where 
+    arbitrary = return Trivial
+
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+type TrivAssoc = Trivial -> Trivial -> Trivial -> Bool
+main :: IO ()
+main = quickCheck (semigroupAssoc :: TrivAssoc)
+
+-- Exer 2. Do the same for this: . newtype Identity a = Identity a
+
+newtype Identity a = Identity a
+
+instance Semigroup a => Semigroup (Identity a) where
+    (<>) (Identity x) _            = Identity x
+    (<>) _ (Identity y)            = Identity y
+    (<>) (Identity x) (Identity y) = Identity (x <> y)
+
+-- instance Arbitrary a => Arbitrary (Identity a) where
+--     arbitrary = return (Identity a)
+
+genInteg :: Arbitrary a => Gen (Maybe a)
+genInteg = do
+    x <- arbitrary
+    frequency [(0, return Nothing), (1, return (Just x))]
+
+checkPositive = Prelude.length autogen
+  where 
+    autoGen     = sample' (genInteg :: Gen (Maybe Int))
+
+checkList = print $ checkPositive
+-- mainTwo :: IO ()
+-- mainTwo = sample' (genInteg :: Gen (Maybe Int))
