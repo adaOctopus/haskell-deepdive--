18895d406f85190505e6d5d2005d810607c50746
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-09-06 16:16:51 +0300

    Added comments functors

--- Haskell Excercises & Code/Chapter16 - Functor/functors.hs
+++ Haskell Excercises & Code/Chapter16 - Functor/functors.hs
@@ -11,4 +11,10 @@ module Functorino where
 -- This concerns the composability of fmap. If we compose two functions, 
 -- 𝑓 and 𝑔, and fmap that over some structure, we should get the same result as if we fmapped them and then composed them:
 
-let replaceWithP = const 'p'
+replaceWithP = const 'p'
+fixedList = [Just 1, Nothing, Just 2]
+
+-- Why (fmap . fmap) replaceWithP fixedList
+-- returns -> [Just 'p', Nothing, Just 'p']
+-- when fmap replaceWithP fixedList simply returns "ppp"
+-- I do not understand the reasoning
