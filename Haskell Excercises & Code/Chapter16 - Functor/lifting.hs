53b8addeee361efbf6df8c0d770a2330f417bddc
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-09-07 11:39:02 +0300

    Main IO for lifting

--- Haskell Excercises & Code/Chapter16 - Functor/lifting.hs
+++ Haskell Excercises & Code/Chapter16 - Functor/lifting.hs
@@ -33,3 +33,22 @@ thriceLifted = (fmap . fmap . fmap) replaceWithP
 thriceLifted' :: [Maybe [Char]] -> [Maybe [Char]]
 thriceLifted' = thriceLifted
 
+
+-- Practice on repl to see
+
+main :: IO ()
+main = do 
+    putStr "replaceWithP' lms: "
+    print (replaceWithP' lms)
+    putStr "liftedReplace lms: "
+    print (liftedReplace lms)
+    putStr "liftedReplace' lms: "
+    print (liftedReplace' lms)
+    putStr "twiceLifted lms: "
+    print (twiceLifted lms)
+    putStr "twiceLifted' lms: "
+    print (twiceLifted' lms)
+    putStr "thriceLifted lms: "
+    print (thriceLifted lms)
+    putStr "thriceLifted' lms: "
+    print (thriceLifted' lms)
