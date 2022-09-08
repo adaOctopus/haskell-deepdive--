18c82ca10292d83bce1f103e5a047cb85d5d9d81
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-09-08 13:06:25 +0300

    5 functor instances

--- Haskell Excercises & Code/Chapter16 - Functor/instances.hs
+++ Haskell Excercises & Code/Chapter16 - Functor/instances.hs
@@ -1,18 +1,34 @@
 module FuncInstance where 
 
 
--- 1. newtype Identity a = Identity a
+-- 1. 
+
+newtype Identity a = Identity a
 
 instance Functor Identity where 
-    fmap id (Identity a) = Identity a
     fmap f (Identity a)  = Identity (f a)
 
 
--- 2. data Pair a = Pair a a
+-- 2. 
+data Pair a = Pair a a
 
 instance Functor Pair where
     fmap f (Pair a a') = Pair (f a) (f a')
 
--- 3. data Two a b = Two a b
--- 4. data Three a b c = Three a b c
--- 5. data Three' a b = Three' a b b
+-- 3. 
+data Two a b = Two a b
+
+instance Functor (Two a) where 
+    fmap f (Two alpha beta) = Two alpha (f beta)
+
+-- 4. 
+data Three a b c = Three a b c
+
+instance Functor (Three a b) where
+    fmap f (Three al be ci) = Three al be (f ci)
+
+-- 5. 
+data Three' a b = Three' a b b
+
+instance Functor (Three' a) where
+    fmap f (Three' a1 b1 b2) = Three' a1 (f b1) (f b2)
