6a73b8f5203d5c7740bd5c975fbf6216ac976106
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-09-12 13:52:00 +0300

    Finished instance validity

--- Haskell Excercises & Code/Chapter16 - Functor/chapterEx.hs
+++ Haskell Excercises & Code/Chapter16 - Functor/chapterEx.hs
@@ -17,13 +17,33 @@ data BoolAndMaybeSomethingElse a = Falsish | Truish a
 newtype Mu f = InF { outF :: f (Mu f) }
 -- Yes it can be
 
-data D = D (Array Word Word) Int Int
+-- data D = D (Array Word Word) Int Int
 -- Nope it is concrete
 
 -- Exer 2. Rearrange arguments so Functor instance works
 
-data Sum a b = First a | Second b
+-- Solution, switched b & a
+data Sum b a = First a | Second b
 
 instance Functor (Sum e) where
     fmap f (First a) = First (f a)
     fmap f (Second b) = Second b
+
+
+-- Solution, switched b & c
+data Company a c b = DeepBlue a c | Something b
+
+instance Functor (Company e e') where 
+    fmap f (Something b) = Something (f b)
+    fmap _ (DeepBlue a c) = DeepBlue a c
+
+
+-- Switched a & b
+data More b a = L a b a | R b a b deriving (Eq, Show)
+
+instance Functor (More x) where -- think like More a
+    fmap f (L a b a') = L (f a) b (f a')
+    fmap f (R b a b') = R b (f a) b'
+
+
+-- Ex.3 Write Functor instances for the data types
