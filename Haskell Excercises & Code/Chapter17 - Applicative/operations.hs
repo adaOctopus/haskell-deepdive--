7b8084d372c3de0c606157538ac509b93b0ce682
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-09-13 14:21:41 +0300

    Lookup exer typechecks

--- Haskell Excercises & Code/Chapter17 - Applicative/operations.hs
+++ Haskell Excercises & Code/Chapter17 - Applicative/operations.hs
@@ -15,3 +15,20 @@ m x = lookup x [(4, 10), (8, 13), (1, 9001)]
 -- how can u get length of julie?
 -- Well done I did it!
 -- pure length <*> liftA2 (++) (g 9) (f 4)
+
+-- Lookup Exer
+-- Make the following expressions typecheck.
+-- 1. SOL: Added pure before +3 and <*>
+added :: Maybe Integer
+added = pure (+3) <*> (lookup 3 $ zip [1, 2, 3] [4, 5, 6])
+
+--2. Changed the tupled type from Maybe (Integer, Integer) to (Maybe Integer, Maybe Integer)
+y :: Maybe Integer
+y = lookup 3 $ zip [1, 2, 3] [4, 5, 6]
+
+z :: Maybe Integer
+z = lookup 2 $ zip [1, 2, 3] [4, 5, 6]
+
+tupled ::  (Maybe Integer, Maybe Integer)
+tupled = (,) y z
+
