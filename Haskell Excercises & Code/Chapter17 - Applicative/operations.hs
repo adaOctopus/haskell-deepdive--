5eaa1e47ac7aae7f476a13fca059f647a6dfee13
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-09-13 14:42:56 +0300

    Finished with loookups

--- Haskell Excercises & Code/Chapter17 - Applicative/operations.hs
+++ Haskell Excercises & Code/Chapter17 - Applicative/operations.hs
@@ -1,6 +1,7 @@
 module Operations where
 
 import Control.Applicative
+import Data.List (elemIndex)
 
 f x = lookup x [ (3, "hello"), (4, "julie"), (5, "kbai")]
 
@@ -31,4 +32,43 @@ z = lookup 2 $ zip [1, 2, 3] [4, 5, 6]
 
 tupled ::  (Maybe Integer, Maybe Integer)
 tupled = (,) y z
+
+-- 3. Added pure max' <*> <*>
+-- Basically for the applicative to make sense we need functions in a STRUCTURE
+-- If the functions are not in a structure, pure dem up!
+
+x :: Maybe Int
+x = elemIndex 5 [1, 2, 3, 4, 5]
+
+n :: Maybe Int
+n = elemIndex 2 [1, 2, 3, 4, 5]
+
+max' :: Int -> Int -> Int
+max' = max
+
+maxed :: Maybe Int
+maxed = pure max' <*> x <*> n
+
+-- 4.
+xs = [1, 2, 3]
+ys = [4, 5, 6]
+
+x' :: Maybe Integer
+x' = lookup 3 $ zip xs ys
+-- this results to
+-- Step 1: [(1,4),(2,5),(3,6)]
+-- Step 2: [(3,6)]
+-- -> Just 6
+
+y' :: Maybe Integer
+y' = lookup 2 $ zip xs ys
+-- this results to
+-- Step 1: [(1,4),(2,5),(3,6)]
+-- Step 2: [(2,5)]
+-- -> Just 5
+-- Super weird i dont gget
+-- summed :: Maybe Integer
+summed = sum <$> (,) x' y'
+
+
 
