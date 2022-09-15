fa226f2f485099aee7cf649d301151686bd186e9
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-09-15 13:30:26 +0300

    Quickcheck tests with applicatives

--- Haskell Excercises & Code/Chapter17 - Applicative/fixerUpper.hs
+++ Haskell Excercises & Code/Chapter17 - Applicative/fixerUpper.hs
@@ -1,6 +1,7 @@
 module FixerUpper where
 
 import Control.Applicative
+import Test.QuickCheck
 -- 1.
 
 -- funca1 :: (Show a, Show b) => f (a -> b) -> Maybe String -> Maybe String 
@@ -11,3 +12,18 @@ funca1 = pure const <*> Just "Hello" <*> Just "World"
 -- 2. 
 -- 
 funca2 = (,,,) <$> Just 90 <*> Just 10 <*> Just "Tierness" <*> pure [1, 2, 3]
+
+-- Define identity laws and test their equality
+law1 = id [1,2,3]
+
+-- Functor
+law2 = fmap id [1,2,3]
+
+-- Applicative
+law3 = pure id <*> [1,2,3]
+
+checkIdent :: (Eq a, Show a, Num a) => [a] -> Bool
+checkIdent xs = id xs == fmap id xs
+
+runQuickCheck :: IO ()
+runQuickCheck = quickCheck (checkIdent :: [Int] -> Bool)
