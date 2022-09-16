639176ade18812deda30c4c43cdd339dc2ae4859
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-09-16 14:02:05 +0300

    Writting instances, not sure for their Accuracy

---
+++ Haskell Excercises & Code/Chapter17 - Applicative/listApplicative.hs
@@ -0,0 +1,16 @@
+module ListApplicative where
+
+data List a = Nil | Cons a (List a) deriving (Eq, Show)
+
+
+-- Remember what you wrote for the list Functor:
+instance Functor List where 
+    fmap f Nil = Nil
+    fmap f (Cons a lista) = Cons (f a) (fmap f lista)
+-- Writing the list Applicative is similar.
+
+instance Applicative List where
+    pure x = Cons x Nil
+    Nil <*> _ = Nil
+    _ <*> Nil = Nil
+    Cons f x <*> y = (f <$> y) <> (x <*> y)
