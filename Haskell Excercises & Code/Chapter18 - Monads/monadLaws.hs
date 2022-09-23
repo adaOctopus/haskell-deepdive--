869b2b36e2e91d04bd91239752ffccdf73332175
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-09-23 13:39:19 +0300

    Monad Laws and Kleisli composition

--- Haskell Excercises & Code/Chapter18 - Monads/monadLaws.hs
+++ Haskell Excercises & Code/Chapter18 - Monads/monadLaws.hs
@@ -1,5 +1,8 @@
 module MonadLaws where
 
+--import Test.QuickCheck
+import Control.Monad ((>=>))
+
 --- IDENTITY LAWS
 -----------------
 
@@ -15,3 +18,23 @@ module MonadLaws where
 --- (m >>= f) >>= g = m >>= (\x -> f x >>= g)
 
 -- mempty = 
+-- Composition type sign
+-- :: (b -> c) -> (a -> b) -> a -> c
+-- Monad type sign
+--(>>=) :: Monad m => m a -> (a -> m b) -> m b
+-- flipped composition type sign
+-- (>=>) :: Monad m => (b -> m c) -> (a -> m b) -> a -> m c
+
+sayHi :: String -> IO String
+sayHi greeting = do
+    putStrLn greeting
+    getLine
+
+readM :: Read a => String -> IO a
+readM = return . read
+
+getAge :: String -> IO Int
+getAge = sayHi >=> readM
+
+askForAge :: IO Int
+askForAge = getAge "Hello! How old are you? "
