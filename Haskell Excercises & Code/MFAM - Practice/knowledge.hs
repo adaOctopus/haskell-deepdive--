881d3b42974e8b1e33526d14cb0279385dad2cef
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-09-24 19:20:51 +0300

    Finished Monoid, Semigroup

--- Haskell Excercises & Code/MFAM - Practice/knowledge.hs
+++ Haskell Excercises & Code/MFAM - Practice/knowledge.hs
@@ -4,6 +4,7 @@ module TestingKnowl where
 -- This is mainly for the revision purposes and testing of my understanding of the following:
 -- Monoids, Functors, Applicatives, Monads -> Instances and their Laws
 -- PS: Do not proceed without testing your knowledge for this
+import Data.Monoid
 
 -- Starting by writing a few dummy data types
 
@@ -28,3 +29,74 @@ data Coin = Coin Name Amount deriving (Eq, Show)
 -- To me , Monoids are contexts or structures to say it better, that can be joined together.
 -- mempty which is basically the identity value, for addition it is 0 for multiplication is 1
 -- mappend which basically combines two same structures together
+
+-- Basically semigroup is mappend without the identity law, mempty missing
+-- class Semigroup a where
+--   (<>) :: a -> a -> a
+
+-- Class definiton for Monoid
+-- Monoid is a semigroup that fulfills the identity law
+-- class Semigroup a => Monoid a where
+--     mempty :: a
+--     mappend :: a -> a -> a
+--     mconcat :: [a] -> a
+
+-- Remember Monoid expects of type * kind when writting instances
+-- However, Functor, Applicative , Monad expect * -> * higher kinded
+-- But we will explain that later in this file.
+
+-- For practice lets right an instance for Monoid for Token and Coin
+-- Remember we cannot write functor or applicative or monad instances
+-- Because they are of concrete type * and not higher kinded, aka. they are no wrapping structures
+
+-- ============================================================================
+--                                  Monoids
+-- ============================================================================
+{--
+-- A Monoid is a binary associative operation with an identity
+-- Laws for Monoids
+-- Left and Right Identity
+mappend mempty x = x
+mappend x mempty = x
+-- Associativity
+mappend x (mappend y z) = mappend (mappend x y) z
+mconcat = foldr mappend mempty
+-}
+
+-- ============================================================================
+--                                Semigroups
+-- ============================================================================
+-- A semigroup is an associative binary operation
+-- (This is a monoid without an identity)
+{-
+class Semigroup a where
+    (<>) :: a -> a -> a
+-- Law:
+(a <> b) <> c == a <> (b <> c)
+-}
+
+-- Semigroups will be coming to base in GHC 8.0, and we will see:
+-- class Semigroup a => Monoid a where
+-- ...
+-- in the definition of Monoid.
+
+-- ========
+-- examples
+-- ========
+
+data MyFlag a = Burned | Moving a deriving (Eq, Show)
+-- lets write monoid instance
+
+instance Semigroup a => Semigroup (MyFlag a) where
+    Burned <> Burned = Burned
+    (Moving a) <> Burned = Moving a
+    Burned <> (Moving a) = Moving a
+
+instance Monoid a => Monoid (MyFlag a) where
+    mempty = Burned
+    mappend Burned Burned = Burned
+    mappend (Moving a) Burned = Moving a
+    mappend Burned (Moving a) = Moving a
+    mappend (Moving a) (Moving b) = Moving (mappend a b)
+
+
