b436dc37954b86939c21dc1c1728320c26479dfd
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-10-01 16:40:51 +0300

    sequenceA & traverse functions reasoning

--- Haskell Excercises & Code/Chapter21 - Traversable/traverse101.hs
+++ Haskell Excercises & Code/Chapter21 - Traversable/traverse101.hs
@@ -21,3 +21,34 @@ module T101 where
 -- traverse
 -- :: (Applicative f, Traversable t)
 -- => (a -> f b) -> t a -> f (t b)
+-- ================================= IMPORTANT NOTES REASON ============
+-- =====================================================================
+-- fmap :: (a -> b) -> f a -> f b
+-- (=<<) :: (a -> m b) -> m a -> m b
+-- traverse :: (a -> f b) -> t a -> f (t b)
+-- =================================================================
+
+-- In fact, as we saw in the typeclass definition, traverse is fmap
+-- composed with sequenceA:
+-- traverse f = sequenceA . fmap 
+-- i.e
+-- fmap Just [1,2,3]
+-- ==> [Just 1, Just 2,Just 3]
+
+-- sequenceA [Just 1, Just 2,Just 3]
+-- ==> Just [1,2,3]
+
+-- so traverse is
+-- traverse Just [1,2,3] == sequenceA . fmap Just $ [1,2,3]
+
+-- =================================================================
+
+-- We’re still mapping a function over some embedded value(s), like
+-- fmap, but similar to flip bind, that function is itself generating
+-- more structure. However, unlike flip bind, that structure can
+-- be of a different type than the structure we lifted over to apply
+-- the function. And at the end, it will flip the two structures
+-- around, as sequenceA did.
+-- In fact, as we saw in the typeclass definition, traverse is fmap
+-- composed with sequenceA:
+-- traverse f = sequenceA . fmap 
