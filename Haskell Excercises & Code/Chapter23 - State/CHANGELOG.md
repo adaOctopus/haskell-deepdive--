a5a6f97d412298cfaf26b0eb70b91c1617ec4f48
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-10-07 15:00:01 +0300

    Testing cabal project with Random library

---
+++ Haskell Excercises & Code/Chapter23 - State/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for Chapter23-State
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ Haskell Excercises & Code/Chapter23 - State/app/StateTest.hs
@@ -0,0 +1,11 @@
+module StateTest where
+import System.Random
+-- Six-sided die
+data Die =
+    DieOne
+    | DieTwo
+    | DieThree
+    | DieFour
+    | DieFive
+    | DieSix
+    deriving (Eq, Show)
