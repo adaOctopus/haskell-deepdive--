f396ef7506ca69e98066fd78b9a9ce115eeb1250
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-10-14 18:30:31 +0300

    Cannot quickcheck to validate laws

--- Haskell Excercises & Code/Chapter25 - Composing Types/EitherTT.hs
+++ Haskell Excercises & Code/Chapter25 - Composing Types/EitherTT.hs
@@ -7,7 +7,10 @@ import Test.QuickCheck
 
 newtype EitherT e m a = EitherT { runEitherT :: m (Either e a) }
 
-newtype TestData a b = TestData b deriving (Eq, Show)
+-- -- 1. Write the Functor instance for EitherT:
+-- instance Functor m => Functor (EitherT e m) where
+--     fmap f (EitherT ema) = EitherT $ (fmap . fmap) f ema
+
 
 -- Github Arbitrary example
 -- instance Arbitrary Cell where
@@ -16,14 +19,33 @@ newtype TestData a b = TestData b deriving (Eq, Show)
 --      Positive y <- arbitrary
 --      return $ Cell x y
 
+newtype TestData a b = TestData b deriving (Eq, Show)
+
+instance Functor (TestData a) where
+    fmap f (TestData b) = TestData (f b)
+
+functorIdentity :: (Functor f, Eq (f a)) => f a -> Bool
+functorIdentity f = fmap id f == f
+
+
 -- arbitrary :: Gen a
 genArbit :: (Arbitrary a, Arbitrary b) => Gen (TestData a b)
 genArbit = do
     x1 <- arbitrary
     return $ TestData x1
 
+instance (Arbitrary a, Arbitrary b) => Arbitrary (TestData a b) where
+    arbitrary = genArbit
+
 randomGen = sample' (genArbit :: Gen (TestData Int Int))
 
--- 1. Write the Functor instance for EitherT:
-instance Functor m => Functor (EitherT e m) where
-    fmap f (EitherT ema) = EitherT $ (fmap . fmap) f ema
+-- validateFunctor :: [TestData Int Int] -> Bool
+-- validateFunctor x = return x >>= f
+--   where
+--     f = fmap id
+
+    -- functorIdentity :: (Functor f, Eq (f a)) =>
+    -- f a
+    -- -> Bool
+    -- functorIdentity f =
+    -- fmap id f == f
