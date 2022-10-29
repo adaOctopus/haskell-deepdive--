c1e3839d1a5a79857e07e274efafa091295b1604
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-10-29 17:42:16 +0300

    Functor & Applicative instances for the Parser datatype

--- Haskell Excercises & Code/Alejandro - Book of Monads/jsonParser.hs
+++ Haskell Excercises & Code/Alejandro - Book of Monads/jsonParser.hs
@@ -1,4 +1,10 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+
+
 module JSONParser where
+
+import Data.Char
 
 newtype Parser a = Parser { runParser :: String -> Maybe (String, a)}
 
@@ -8,3 +14,17 @@ charP x = Parser $ \input ->
         y:ys | y == x      -> Just (ys, x)
              | otherwise   -> Nothing
         []                 -> Nothing
+
+-- Write a functor instance for the Parse
+
+instance Functor Parser where
+    fmap f (Parser rp) = Parser $ \s -> do
+                          (s', x) <- rp s
+                          Just (s', f x)
+
+instance Applicative Parser where
+    pure x = Parser $ \input -> Just (input, x)
+    Parser fz <*> Parser rp = Parser $ \input -> do
+                               (input' , x) <- rp input
+                               (input'', aTob) <- fz input'
+                               Just (input'', aTob x)
