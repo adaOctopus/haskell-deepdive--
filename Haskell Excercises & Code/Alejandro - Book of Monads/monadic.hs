ad8990cb65d0311d425f234bba1750543c11cd9e
Anastasios Valtinos <anastasios.valtinos@trint.com>
2022-10-31 16:40:32 +0200

    Writting fmap with ap and pure

--- Haskell Excercises & Code/Alejandro - Book of Monads/monadic.hs
+++ Haskell Excercises & Code/Alejandro - Book of Monads/monadic.hs
@@ -6,8 +6,14 @@ import Control.Monad.State as CMS
 
 -- implement `ap` yourself
 
-ap :: Monad m => m (a -> b) -> m a -> m b
-ap mf mx = do
+ap'' :: Monad m => m (a -> b) -> m a -> m b
+ap'' mf mx = do
     f <- mf
     x <- mx
-    return (f x) 
+    return (f x)
+
+
+-- Another exercise. Write fmap with ap & pure
+
+-- fmap'' :: Functor f => (a -> b) -> f a -> f b
+fmap'' f a = pure f `ap` a
